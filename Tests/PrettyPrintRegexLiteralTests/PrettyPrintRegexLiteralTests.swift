0865c76439595b21433ef0c64171893703e4dcb2
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-02-18 22:06:24 +0100

    Add test cases for simple literals
    Adds test cases for:
    - "/abc/"
    - "a(b)"
    - "/(())/"
    - "/a(b)(cd(ef))g/"

--- Tests/PrettyPrintRegexLiteralTests/PrettyPrintRegexLiteralTests.swift
+++ Tests/PrettyPrintRegexLiteralTests/PrettyPrintRegexLiteralTests.swift
@@ -34,6 +34,50 @@ final class PrettyPrintRegexLiteralTests: XCTestCase {
 		#endif
 	}
 
+	///Test if simple literals are working
+	///e.g. "abc/" => "/abc/"
+	func testSimpleLiterals() throws {
+		// Some of the APIs that we use below are available in macOS 10.13 and above.
+		guard #available(macOS 10.13, *) else {
+			return
+		}
+		
+		#if !targetEnvironment(macCatalyst)
+		let inputOutput: [(in: String, out: String)] = [
+			("/abc/", "let literal = /abc/\n"),
+			("a(b)", """
+					let literal = /a(b)/
+					//              ^--  > (b)\n
+					"""),
+			
+			("/(())/", """
+						let literal = /(())/
+						//             ^---  > (())
+						//              ^-   > ()\n
+						"""),
+			("/a(b)(cd(ef))g/", """
+								let literal = /a(b)(cd(ef))g/
+								//                 ^-------   > (cd(ef))
+								//                    ^---    > (ef)
+								//              ^--           > (b)\n
+								""")
+		]
+		let fooBinary = productsDirectory.appendingPathComponent("PrettyPrintRegexLiteral")
+		for test in inputOutput{
+			let process = Process()
+			process.executableURL = fooBinary
+			process.arguments = [test.in]
+			let pipe = Pipe()
+			process.standardOutput = pipe
+			try process.run()
+			process.waitUntilExit()
+			let data = pipe.fileHandleForReading.readDataToEndOfFile()
+			let output = String(data: data, encoding: .utf8)
+			XCTAssertEqual(output, test.out, "failed at: \(test.in)")
+		}
+		#endif
+	}
+	
     /// Returns path to the built products directory.
     var productsDirectory: URL {
       #if os(macOS)
