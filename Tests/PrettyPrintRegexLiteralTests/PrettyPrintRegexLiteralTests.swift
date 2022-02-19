7c3b084783a50ae7d85f75860371007a2063e920
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-02-19 21:16:10 +0100

    Add test cases for quantifiers
    Adds test cases for quantifiers:
    e.g.
    $PrettyPrintRegexLiteral "/(()**)?+/"
    let literal = /(()**)?+/
    //             ^-------  > (()**)?+
    //              ^--      > ()*

--- Tests/PrettyPrintRegexLiteralTests/PrettyPrintRegexLiteralTests.swift
+++ Tests/PrettyPrintRegexLiteralTests/PrettyPrintRegexLiteralTests.swift
@@ -77,6 +77,51 @@ final class PrettyPrintRegexLiteralTests: XCTestCase {
 		}
 		#endif
 	}
+    
+    ///Test if quantifiers after literals are working
+    /// "/a(b)+?"
+    func testLiteralQuantifiers() throws {
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+        
+        #if !targetEnvironment(macCatalyst)
+        let inputOutput: [(in: String, out: String)] = [
+            ("/abc*?/", "let literal = /abc*?/\n"),
+            ("a(b)+", """
+                    let literal = /a(b)+/
+                    //              ^---  > (b)+\n
+                    """),
+            
+            ("/(()**)?+/", """
+                        let literal = /(()**)?+/
+                        //             ^-------  > (()**)?+
+                        //              ^--      > ()*\n
+                        """),
+            ("/a(b)??(cd(ef))a??g/", """
+                                let literal = /a(b)??(cd(ef))a??g/
+                                //                   ^-------      > (cd(ef))
+                                //                      ^---       > (ef)
+                                //              ^----              > (b)??\n
+                                """)
+        ]
+        let fooBinary = productsDirectory.appendingPathComponent("PrettyPrintRegexLiteral")
+        for test in inputOutput{
+            let process = Process()
+            process.executableURL = fooBinary
+            process.arguments = [test.in]
+            let pipe = Pipe()
+            process.standardOutput = pipe
+            try process.run()
+            process.waitUntilExit()
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            let output = String(data: data, encoding: .utf8)
+            print(output)
+            XCTAssertEqual(output, test.out, "failed at: \(test.in)")
+        }
+        #endif
+    }
 	
     /// Returns path to the built products directory.
     var productsDirectory: URL {
